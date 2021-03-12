fca175b91917c8a5e697493fa60cb13acf7640f1
graemenail <graemenail@users.noreply.github.com>
2021-03-12 09:45:16 +0000

    deploy: ec51abe9cb90a2353cffbc6f8a83a88df4e3b623

--- docs/docs/_static/documentation_options.js
+++ docs/docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.10.5 2021-03-10',
+    VERSION: 'v1.10.5 2021-03-12',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
